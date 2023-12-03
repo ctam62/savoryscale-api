439286e3519f857b05f67988e1918ecf1133e99b
claratam <claramtam@gmail.com>
2023-12-03 12:17:40 -0600

    refactor: change client to postgressql

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-  client: 'mysql2',
+  client: 'pg',
   connection: {
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
@@ -9,4 +9,21 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     charset: 'utf8',
   },
+
+  onUpdateTimestampFunction: `
+    CREATE OR REPLACE FUNCTION on_update_timestamp()
+    RETURNS trigger AS $$
+    BEGIN
+      NEW.updated_at = now();
+      RETURN NEW;
+    END;
+    $$ language 'plpgsql';
+`,
+
+  onUpdateTrigger: table => `
+    CREATE TRIGGER ${table}_updated_at
+    BEFORE UPDATE ON ${table}
+    FOR EACH ROW
+    EXECUTE PROCEDURE on_update_timestamp();
+  `
 };
